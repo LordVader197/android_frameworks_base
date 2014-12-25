751a1e35340141d807255a033fc53d217f6f667f
Lars Greiss <kufikugel@googlemail.com>
2014-12-25 06:56:36 +0000

    HeadsUp: Swipe options
    -left swipe dismiss
    -right swipe put into background
    -swipe down opens notification drawer and moves the notification into it
    
    Signed-off-by: OwnDroid <askowndroid@gmail.com>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
    
    Change-Id: I34570267a3eb9f58edcf3a6521335b134f8792f3

--- packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
@@ -58,6 +58,7 @@ public class CommandQueue extends IStatusBar.Stub {
     private static final int MSG_NOTIFICATION_LIGHT_OFF             = 16 << MSG_SHIFT;
     private static final int MSG_NOTIFICATION_LIGHT_PULSE           = 17 << MSG_SHIFT;
     private static final int MSG_START_CUSTOM_INTENT_AFTER_KEYGUARD = 18 << MSG_SHIFT;
+    private static final int MSG_HIDE_HEADS_UP                      = 19 << MSG_SHIFT;
 
     public static final int FLAG_EXCLUDE_NONE = 0;
     public static final int FLAG_EXCLUDE_SEARCH_PANEL = 1 << 0;
@@ -100,6 +101,7 @@ public class CommandQueue extends IStatusBar.Stub {
         public void notificationLightOff();
         public void notificationLightPulse(int argb, int onMillis, int offMillis);
         public void showCustomIntentAfterKeyguard(Intent intent);
+        public void scheduleHeadsUpClose();
     }
 
     public CommandQueue(Callbacks callbacks, StatusBarIconList list) {
@@ -251,6 +253,13 @@ public class CommandQueue extends IStatusBar.Stub {
         }
     }
 
+    public void scheduleHeadsUpClose() {
+        synchronized (mList) {
+            mHandler.removeMessages(MSG_HIDE_HEADS_UP);
+            mHandler.sendEmptyMessage(MSG_HIDE_HEADS_UP);
+        }
+    }
+
     private final class H extends Handler {
         public void handleMessage(Message msg) {
             final int what = msg.what & MSG_MASK;
@@ -333,8 +342,10 @@ public class CommandQueue extends IStatusBar.Stub {
                 case MSG_START_CUSTOM_INTENT_AFTER_KEYGUARD:
                     mCallbacks.showCustomIntentAfterKeyguard((Intent) msg.obj);
                     break;
+                case MSG_HIDE_HEADS_UP:
+                    mCallbacks.scheduleHeadsUpClose();
+                    break;
             }
         }
     }
 }
-

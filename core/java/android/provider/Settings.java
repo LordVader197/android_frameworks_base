4768971622bb953d0c6c888796effe12a1825a3d
OwnDroid <askowndroid@gmail.com>
2015-01-08 16:08:36 +0100

    Customizable Recents Clear All Buton 2/2
            - Material Dark Themed
    
    Change-Id: I0ea161b478c6a64a78ce71a61bc1b992acf5294a
    Signed-off-by: OwnDroid <askowndroid@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3412,6 +3412,17 @@ public final class Settings {
 		 */
 		 public static final String CLOCK_USE_SECOND = "clock_use_second";
 
+	   /**
+		* show clear all recents button
+		* @hide
+		*/
+		public static final String SHOW_CLEAR_ALL_RECENTS = "show_clear_all_recents";
+
+	   /**
+		* location of the clear all rectents button
+		* @hide
+		*/
+		public static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
 
 		 /**
          * Settings to backup. This is here so that it's in the same place as the settings
